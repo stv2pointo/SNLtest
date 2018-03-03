75f3cbc992d62b97f67588a0eb561a3e91e38e8a
Steve Julien <stevenjulien@mail.weber.edu>
2018-03-03 12:04:19 -0700

    Reads Employees.txt

--- SteveJulienSNLtest/Program.cs
+++ SteveJulienSNLtest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,39 @@ namespace SteveJulienSNLtest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            string[] lines = readFromFile();
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
             Console.ReadLine();
+        }
+
+        public static string[] readFromFile()
+        {
+            int counter = 0;
+            string line;
+            List<string> fileLines = new List<string>();
+            //string path = @"c:\SecurityNational\Employees.txt";
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\Resources\Employees.txt");
+            try
+            {
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(path);
+                while ((line = file.ReadLine()) != null && counter < 10)
+                {
+                    fileLines.Add(line);
+                    counter++;
+                }
+
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("bad path: " + e.Message);
+            }
+
+            return fileLines.ToArray();
         }
     }
 }
